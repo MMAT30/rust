bfbf534434c46b14caaef939de58522648c2c67e
anonymous <mateo.martinez.spc@icloud.com>
2024-01-01 16:21:56 -0600

    content

--- 010-options/src/main.rs
+++ 010-options/src/main.rs
@@ -1,45 +1,120 @@
 
-// fn main() {
+#[derive(Debug)]
+enum IpAddrKind {
+    
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
 
-//     // running functions
-//     try_division(4, 2);
-//     try_division(1, 0);
-
-
-
-//     // creating a none option in two ways
-//     let none: Option<i32> = None;
-//     println!("{:?}", none);
-
-//     let none = None::<i32>;
-//     println!("{:?}", none);
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind
+}
 
 
-//     // creating a some option to show case the unwrap method
-//     let opt_float = Some(0f32);
-//     println!("{:?}", opt_float);
 
-//     // unwrapping a some will return the value
-//     println!("{:?} unwraps to {:?}", opt_float, opt_float.unwrap());
+// the advatage of have a enum that holds diffrent types
+// is because the enum type encompases all the types
+// meaning the all enums are of type Message
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
-//     // unwrapping a none will panic
-//     // println!("{:?} unwraps to {:?}", none, none.unwrap());
-// }
+#[derive(Debug)]
+enum States {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(States),
+}
+
+impl Message {
+    fn call(&self){
+        println!("call");
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Penny");
+            1
+        },
+        Coin::Nickel => {
+            println!("Nickel");
+            5
+        },
+        Coin::Dime => {
+            println!("Dime");
+            10
+        },
+        Coin::Quarter(s) => {
+            println!("Quarter from {:?}",s);
+            25
+        },
+    }
+}
+
+fn main() {
+
+    let four = IpAddrKind::V4(127,0,0,1);
+    let six = IpAddrKind::V6("::1".to_string());
+
+    println!("four: {:?}", four);
+    println!("six: {:?}", six);
 
 
-// fn checked_division(dividend: i32, divisor: i32) -> Option<i32> {
-//     if divisor == 0 {
-//         None
-//     } else {
-//         Some(dividend / divisor)
-//     }
-// }
 
-// fn try_division(dividend: i32, divisor: i32) {
-//     match checked_division(dividend, divisor) {
-//         None => println!("{} / {} failed!", dividend, divisor),
-//         Some(quotient) => {
-//             println!("{} / {} = {}", dividend, divisor, quotient)
-//         },
-//     }
-// }
+    // creating instance of struct
+    let home = IpAddr {
+        kind: IpAddrKind::V4(127,0,0,1),
+    
+    };
+
+    println!("home: {:?}", home.kind);
+
+    
+    // 3 ways to create Option enum
+    let n1: Option<i32> = Option::Some(5);
+    let n2: Option<i32> = Some(5);
+    let n3: Option<i32> = None;
+
+
+    // we want to add n1 and n3
+    // but we can't because n3 is None
+    
+
+    // this will crash the program at compile time
+    // let sum: i32 = n1.unwrap() + n3.unwrap();
+
+    // using a conditional unwrap
+    let sum: i32 = n1.unwrap() + n3.unwrap_or(0);
+    println!("sum: {}", sum);
+
+
+    
+    // using match
+    let amt = value_in_cents(Coin::Penny);
+    println!("amt: {}", amt);
+
+
+    let amt = value_in_cents(Coin::Quarter(States::Alaska));
+    println!("amt: {}", amt);
+
+    
+}
+
+
+
+
